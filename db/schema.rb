8389e1eb8d784b33291754b50532ee8f35d8513c
Nagahora Toyoaki <hot87hara@gmail.com>
2017-09-10 15:59:01 +0900

    database utf8mb4 and new password

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 20170718120253) do
 
-  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
     t.string   "name"
     t.string   "email"
     t.datetime "created_at", null: false
